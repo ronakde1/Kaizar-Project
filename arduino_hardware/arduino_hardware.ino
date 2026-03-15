4c5e8d877055ca1e8bb3c73b919a8adbc33ca7eb
ryang <ryang2856@gmail.com>
2026-03-15 12:34:23 +0000

    Finished arduino sketch

--- arduino_hardware/arduino_hardware.ino
+++ arduino_hardware/arduino_hardware.ino
@@ -1,37 +1,37 @@
 #include <DHT.h>
 
-#define TRIG_PIN      7
-#define ECHO_PIN      6
-#define DHT_PIN       5
-#define DHT_TYPE      DHT11
-#define SOUND_PIN     8
-#define MOTOR_PIN     4
-#define LED_PROX      9
-#define LED_TEMP      2
-#define LED_NOISE     3
+#define TRIG_PIN 7
+#define ECHO_PIN 6
+#define DHT_PIN 5
+#define DHT_TYPE DHT11
+#define SOUND_PIN 8
+#define MOTOR_PIN 4
+#define LED_PROX 9
+#define LED_TEMP 2
+#define LED_NOISE 3
 
-const int PROX_CM     = 10;
-const int TEMP_WARN_C = 28;
+const int PROX_CM = 30;
+const int TEMP_WARN_C = 27.5;
 
 const unsigned long SENSOR_INTERVAL = 1000;
 unsigned long lastRead = 0;
 
-const int PULSE_ON    = 150;
-const int PULSE_OFF   = 150;
+const int PULSE_ON = 150;
+const int PULSE_OFF = 150;
 const int PULSE_COUNT = 3;
 
 struct Alert {
-  int  pin;
-  int  pin2;
+  int pin;
+  int pin2;
   bool active;
-  int  pulsesDone;
+  int pulsesDone;
   bool pulseHigh;
   unsigned long pulseUntil;
 };
 
-Alert alertProx  = { LED_PROX,  MOTOR_PIN, false, 0, false, 0 };
-Alert alertTemp  = { LED_TEMP,  -1,        false, 0, false, 0 };
-Alert alertNoise = { LED_NOISE, -1,        false, 0, false, 0 };
+Alert alertProx = { LED_PROX, MOTOR_PIN, false, 0, false, 0 };
+Alert alertTemp = { LED_TEMP, -1, false, 0, false, 0 };
+Alert alertNoise = { LED_NOISE, -1, false, 0, false, 0 };
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -46,16 +46,16 @@ void pinWrite(Alert &a, int val) {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(TRIG_PIN,  OUTPUT);
-  pinMode(ECHO_PIN,  INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   pinMode(SOUND_PIN, INPUT);
   pinMode(MOTOR_PIN, OUTPUT);
-  pinMode(LED_PROX,  OUTPUT);
-  pinMode(LED_TEMP,  OUTPUT);
+  pinMode(LED_PROX, OUTPUT);
+  pinMode(LED_TEMP, OUTPUT);
   pinMode(LED_NOISE, OUTPUT);
   digitalWrite(MOTOR_PIN, LOW);
-  digitalWrite(LED_PROX,  HIGH);
-  digitalWrite(LED_TEMP,  HIGH);
+  digitalWrite(LED_PROX, HIGH);
+  digitalWrite(LED_TEMP, HIGH);
   digitalWrite(LED_NOISE, HIGH);
   dht.begin();
   Serial.println(F("distance_cm,temp_c,loud"));
@@ -66,29 +66,38 @@ void loop() {
 
   while (Serial.available() > 0) {
     char cmd = Serial.read();
-    if      (cmd == '1') digitalWrite(MOTOR_PIN, HIGH);
-    else if (cmd == '0') digitalWrite(MOTOR_PIN, LOW);
+    if (cmd == '1') {
+      digitalWrite(MOTOR_PIN, HIGH);
+      digitalWrite(LED_PROX, LOW);
+      digitalWrite(LED_TEMP, LOW);
+      digitalWrite(LED_NOISE, LOW);
+    } else if (cmd == '0') {
+      digitalWrite(MOTOR_PIN, LOW);
+      digitalWrite(LED_PROX, HIGH);
+      digitalWrite(LED_TEMP, HIGH);
+      digitalWrite(LED_NOISE, HIGH);
+    }
   }
 
-  updateAlert(alertProx,  now);
-  updateAlert(alertTemp,  now);
+  updateAlert(alertProx, now);
+  updateAlert(alertTemp, now);
   updateAlert(alertNoise, now);
 
   if (now - lastRead >= SENSOR_INTERVAL) {
     lastRead = now;
 
     float distCm = measureDistance();
-    float tempC  = dht.readTemperature();
-    bool  loud   = (digitalRead(SOUND_PIN) == HIGH);
+    float tempC = dht.readTemperature();
+    bool loud = (digitalRead(SOUND_PIN) == HIGH);
 
-    if (distCm > 0 && distCm <= PROX_CM)      startAlert(alertProx,  now);
-    if (!isnan(tempC) && tempC >= TEMP_WARN_C) startAlert(alertTemp,  now);
-    if (loud)                                  startAlert(alertNoise, now);
+    if (distCm > 0 && distCm <= PROX_CM) startAlert(alertProx, now);
+    if (!isnan(tempC) && tempC >= TEMP_WARN_C) startAlert(alertTemp, now);
+    if (loud) startAlert(alertNoise, now);
 
     Serial.print(distCm, 1);
     Serial.print(F(","));
     if (isnan(tempC)) Serial.print(F("-99"));
-    else              Serial.print(tempC, 1);
+    else Serial.print(tempC, 1);
     Serial.print(F(","));
     Serial.println(loud ? 1 : 0);
   }
@@ -96,9 +105,9 @@ void loop() {
 
 void startAlert(Alert &a, unsigned long now) {
   if (a.active) return;
-  a.active     = true;
+  a.active = true;
   a.pulsesDone = 0;
-  a.pulseHigh  = true;
+  a.pulseHigh = true;
   a.pulseUntil = now + PULSE_ON;
   digitalWrite(a.pin, LOW);
   if (a.pin2 != -1) digitalWrite(a.pin2, HIGH);
@@ -116,10 +125,10 @@ void updateAlert(Alert &a, unsigned long now) {
       a.active = false;
       return;
     }
-    a.pulseHigh  = false;
+    a.pulseHigh = false;
     a.pulseUntil = now + PULSE_OFF;
   } else {
-    a.pulseHigh  = true;
+    a.pulseHigh = true;
     a.pulseUntil = now + PULSE_ON;
     digitalWrite(a.pin, LOW);
     if (a.pin2 != -1) digitalWrite(a.pin2, HIGH);
